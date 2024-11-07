dd254ceeb13c97c4812339e0448b03462e8e6390
azizsas <bensassimohamedaziz7@gmail.com>
2024-11-07 17:37:52 +0100

    Add files via upload

---
+++ src/Components/DataContext.js
@@ -0,0 +1,59 @@
+import { createContext, useContext } from 'react';
+import { player_prop_markets } from "../Resources.js";
+import { useQuery } from "@tanstack/react-query";
+
+import football_data from './../SampleData/americanfootball_nfl_player_props.json';
+import basketball_data from './../SampleData/basketball_nba_player_props.json';
+import baseball_data from './../SampleData/baseball_mlb_player_props.json';
+import hockey_data from './../SampleData/hockey_nhl_player_props.json';
+
+const DataContext = createContext();
+
+export function useData() {
+  return useContext(DataContext);
+}
+
+export const DataProvider = (event) => {
+  let curr_sport = player_prop_markets.filter(sport => sport["label"] === event.sport)[0];
+  const specMarketsForSport = curr_sport["team_markets"] + curr_sport["player_markets"];
+
+  const fetchData = async () => {
+    let odds;
+    if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+      if(event.sport === 'americanfootball_nfl') odds = football_data;
+      else if(event.sport === 'baseball_mlb') odds = baseball_data;
+      else if(event.sport === 'basketball_nba') odds = basketball_data;
+      else odds = hockey_data;
+    }
+    else {
+      const url = 'https://' + process.env.REACT_APP_AWS_API_ID + '.execute-api.' + process.env.REACT_APP_AWS_API_REGION + '.amazonaws.com/default/player-data-fetch?sport=' + event.sport + '&game_id=' + event.game_id + '&specMarkets=' + specMarketsForSport;
+      const playerData = await fetch(url, {
+        method: 'GET'
+      });
+      if (!playerData.ok) {
+        throw new Error(playerData.status, playerData.statusText);
+      }
+      odds = await playerData.json();
+    }
+
+    return odds;
+  };
+
+  const { data, status } = useQuery([event.sport + ' - ' + event.game_id], fetchData,
+    {
+      staleTime: 300000,
+      refetchOnWindowFocus: true,
+      retry: 2
+    }
+);
+
+  
+            
+  return (
+    <DataContext.Provider value={{ data, status }}>
+      {event.showChild && event.children}
+    </DataContext.Provider>
+  );
+}
+
+export default DataContext;
---
+++ src/Components/PopupComponent.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import Popup from 'reactjs-popup';
+import InstructionCarousel from './InstructionCarousel';
+import SportsBookList from './SportsBookList';
+import Policy from './Policy';
+
+const PopupComponent = (type) => {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  let title;
+  if(type.type === "instructions") title = "How to Use"
+  else if(type.type === "sportsbook-list") title = "Sportsbooks"
+  else if(type.type === "privacy") title = "Privacy Policy"
+  else if(type.type === "terms") title = "Terms of Use";
+
+  const handleClick = () => {
+    setIsPopupOpen(true);
+  };
+
+    return (
+      <span>
+          
+          <span className={type.text} onClick={handleClick}>{title}</span>
+          
+          <div className={isPopupOpen ? "overlay" : ""}>
+              <Popup
+                  open={isPopupOpen}
+                  onClose={() => setIsPopupOpen(false)}
+              >
+                {type.type === "instructions" ?
+                <div>
+                  <div className="lg:hidden w-80 h-[40rem]">
+                      <InstructionCarousel size="small"></InstructionCarousel>
+                  </div>
+                  <div className="hidden lg:block max-w-screen-md h-full">
+                      <InstructionCarousel size="big"></InstructionCarousel>
+                  </div>
+                </div> : type.type === "sportsbook-list" ?
+                <div>
+                  <SportsBookList></SportsBookList>
+                </div> : 
+                <div>
+                  <Policy title={title}></Policy>
+                </div>}
+              </Popup>
+          </div>
+          
+      </span>
+    );
+  
+};
+
+export default PopupComponent;
